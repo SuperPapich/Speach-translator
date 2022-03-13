1f9353fbd47e3ce2f41d9e34ea8f26cfc22dde43
Павел Пашута <popashuta@miem.hse.ru>
2022-03-14 00:15:50 +0300

    first commit

---
+++ appserver.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, redirect
+
+import json
+import time
+
+from vosk import Model, KaldiRecognizer
+import wave
+import re
+
+app = Flask(__name__)
+app.secret_key = 'secret key'
+
+
+@app.route('/', methods=['POST', 'GET'])
+def start():
+    return render_template('home.html')
+
+
+
+@app.route('/translate', methods=['POST'])
+def translate():
+
+    model = Model("vosk-model-small-ru-0.22")
+
+    output = 'putin.wav'
+
+    wf = wave.open(output, "rb")
+    rcgn_fr = wf.getframerate() * wf.getnchannels()
+    rec = KaldiRecognizer(model, rcgn_fr)
+    result = ''
+    last_n = False
+    # read_block_size = 4000
+    read_block_size = wf.getnframes()
+    while True:
+        data = wf.readframes(read_block_size)
+        if len(data) == 0:
+            break
+
+        if rec.AcceptWaveform(data):
+            res = json.loads(rec.Result())
+
+            if res['text'] != '':
+                result = result + " " + res['text']
+                if read_block_size < 200000:
+                    print(res['text'] + " \n")
+
+                last_n = False
+            elif not last_n:
+                result += '\n'
+                last_n = True
+
+    res = json.loads(rec.FinalResult())
+    result = result + " " + res['text']
+
+
+
+
+
+
+    return '\n'.join(line.strip() for line in re.findall(r'.{1,150}(?:\s+|$)', result))
